f8d22f70206805a71a007ea3628a0b4803e09869
santosh <santosh@nexxt.in>
2023-04-12 11:02:59 -0400

    removed scikit_learn and pytorch dependency

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup
 
 
-setup(name='simalign',
-      version='0.2',
-      description='Word Alignments using Pretrained Language Models',
-      keywords="NLP deep learning transformer pytorch BERT Word Alignment",
-      url='https://github.com/cisnlp/simalign',
-      author='Masoud Jalili Sabet, Philipp Dufter',
-      author_email='philipp@cis.lmu.de,masoud@cis.lmu.de',
-      license='MIT',
-      packages=['simalign'],
-      install_requires=[
-          "numpy==1.20.3",
-          "torch@https://download.pytorch.org/whl/cpu/torch-1.5.0%2Bcpu-cp38-cp38-linux_x86_64.whl",
-          "scipy==1.6.3",
-          "transformers==4.6.1",
-          "regex",
-          "networkx==2.4",
-          "scikit_learn==0.24.2",
-      ],
-      python_requires=">=3.6.0",
-      zip_safe=False)
+setup(
+    name="simalign",
+    version="0.3",
+    description="Word Alignments using Pretrained Language Models",
+    keywords="NLP deep learning transformer pytorch BERT Word Alignment",
+    url="https://github.com/cisnlp/simalign",
+    author="Masoud Jalili Sabet, Philipp Dufter",
+    author_email="philipp@cis.lmu.de,masoud@cis.lmu.de",
+    license="MIT",
+    packages=["simalign"],
+    install_requires=[
+        "numpy==1.20.3",
+        "scipy==1.6.3",
+        "transformers==4.27.4",
+        "regex",
+        "networkx==2.4",
+        # "scikit_learn==0.24.2",
+    ],
+    python_requires=">=3.6.0",
+    zip_safe=False,
+)
